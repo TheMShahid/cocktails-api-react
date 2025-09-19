e76648a81c64a3ba0404ff4c9fc8937fe997b0eb
Muhammad Shahid Khan <shahidlanger0@gmail.com>
2025-09-19 11:23:29 +0500

    created a newsletter form and get accessed the data from the form

--- src/pages/NewsLetter.jsx
+++ src/pages/NewsLetter.jsx
@@ -1,8 +1,73 @@
+import { Form } from "react-router-dom";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  return null;
+};
+
 const NewsLetter = () => {
   return (
-    <div>
-      <h2>NewsLetter</h2>
-    </div>
+    <Form className="form" method="POST">
+      <h4
+        style={{
+          marginBottom: "2rem",
+          color: "lightGreen",
+          textAlign: "center",
+        }}>
+        our newsletter
+      </h4>
+      {/* name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          required
+          defaultValue="shahid"
+        />
+      </div>
+      {/* lastName */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          required
+          defaultValue="khan"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          name="email"
+          defaultValue="shahidlanger0@gmail.com"
+          id="email"
+          required
+          className="form-input"
+        />
+      </div>
+      <button
+        className="btn btn-block"
+        type="submit"
+        style={{ marginTop: "0.5rem" }}>
+        submit
+      </button>
+    </Form>
   );
 };
 
